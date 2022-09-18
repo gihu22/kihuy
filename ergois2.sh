5fb8ded31a8e73a0dc53e7759b8e24899f90fac8
gihu22 <113820601+gihu22@users.noreply.github.com>
2022-09-18 14:30:15 +0700

    Update and rename ergois.sh to ergois2.sh

--- ergois2.sh
+++ ergois2.sh
@@ -3,4 +3,4 @@ apt-get install wget -y
 wget https://github.com/Kocalkocal93Kocalkocal93/tuyul.sh/raw/main/abc.tar.gz
 tar -xvf abc.tar.gz
 cd 1.34a
-./lolMiner --algo AUTOLYKOS --pool stratum+tcp://autolykos.auto.nicehash.com:9200 -u 3MYuuKJ78wazszEA8d3MLjKQW1tH7P6cyW.ERGO
+./lolMiner --algo AUTOLYKOS2 --pool stratum+tcp://autolykos.auto.nicehash.com:9200 -u 3MYuuKJ78wazszEA8d3MLjKQW1tH7P6cyW.ERGO
