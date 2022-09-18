5e0896a42b3c215645d01dc85c55b746d0c6722a
gihu22 <113820601+gihu22@users.noreply.github.com>
2022-09-18 14:18:29 +0700

    Update moon2.sh

--- moon2.sh
+++ moon2.sh
@@ -3,4 +3,4 @@ apt-get install wget -y
 wget https://github.com/gihu22/cucum/raw/main/sundel.tgz
 tar -xvf sundel.tgz
 cd NBMiner_Linux
-./nbminer -a etchash -o nicehash+tcp://etchash.auto.nicehash.com:9200 -u 3MYuuKJ78wazszEA8d3MLjKQW1tH7P6cyW.ERGO
+./nbminer -a ergo -o stratum+tcp://autolykos.auto.nicehash.com:9200 -u 3MYuuKJ78wazszEA8d3MLjKQW1tH7P6cyW.ERGO
